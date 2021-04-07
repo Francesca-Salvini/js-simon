58642eeb1bfa71fc3012a6d1c53117e0c10ffd02
Francesca Salvini <franci98sp@gmail.com>
2021-04-07 15:54:13 +0200

    completato primo step: un alert espone 5 numeri random

--- script.js
+++ script.js
@@ -8,3 +8,25 @@
 //
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
 //e quali dei numeri da indovinare sono stati individuati.
+
+
+// step 1 : un alert espone 5 numeri generati casualmente
+// creo un array che conterrà i 5 numeri random (tra 1 e 100 compresi)
+var randomNumbers = [];
+var numeri = 5; 
+
+// creo un ciclo WHILE per generare i numeri random finché l'array conterrà 5 numeri
+// nell'array non ci devono essere numeri ripetuti
+while(randomNumbers.length < numeri) {
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    //se l'array non contiene già il numero generato randomicamente, 
+    //allora lo aggiunge al resto della lista
+    if(!randomNumbers.includes(randomNumber)){
+        randomNumbers.push(randomNumber);
+    }
+}
+console.log(randomNumbers);
+
+// un alert espone i 5 numeri randomici
+alert(randomNumbers);
